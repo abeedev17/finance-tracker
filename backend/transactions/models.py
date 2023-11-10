343dfe3828a0c34a6b2e85964602adeaa727b853
abeedev17 <as.asuperstar05@gmail.com>
2023-11-10 09:48:46 +0300

    Define the Transactions Model

--- backend/transactions/models.py
+++ backend/transactions/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Transactions(models.Model):
+    id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=250)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    date = models.DateField()
+    categories = models.ForeignKey('Categories', on_delete=models.CASCADE)
